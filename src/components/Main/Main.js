8b6ed07c145f1664ae479151c961a32abff3c19f
Andrew KIl <kil.andrew@gmail.com>
2020-06-23 10:34:30 -0400

    add Main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div id="main" className="main-container">
+      <a href="#about">
+        <div className="main-title">
+          <h1>ANDREW KIL</h1>
+          <h2>SOFTWARE ENGINEER</h2>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default Main;
