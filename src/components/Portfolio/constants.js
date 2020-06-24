f828cbcbc0154f81cd27a3b41b3c4046576f502d
Andrew KIl <kil.andrew@gmail.com>
2020-06-24 12:01:36 -0400

    add/edit constants

--- src/components/Portfolio/constants.js
+++ src/components/Portfolio/constants.js
@@ -1,6 +1,7 @@
 export const SHOW_ALL = "Show All";
 export const HTML5 = "HTML5";
 export const CSS3 = "CSS3";
+export const HTML5_CSS3 = "HTML5/CSS3";
 export const CSS_MODULES = "CSS Modules";
 export const SASS_SCSS = "SASS/SCSS";
 export const JAVASCRIPT = "JavaScript";
@@ -10,6 +11,7 @@ export const CONTEXT_API = "Context API";
 export const LOCAL_STORAGE = "Local Storage";
 export const NODE = "Node";
 export const EXPRESS = "Express";
+export const NODE_EXPRESS = "Node/Express";
 export const POSTGRESQL = "PostgreSQL";
 export const JEST = "Jest";
 export const MATERIAL_UI = "Material-UI";
@@ -18,64 +20,85 @@ export const CHART_JS = "Chart.js";
 export const NETLIFY = "Netlify";
 export const HEROKU = "Heroku";
 export const FIREBASE = "Firebase";
+export const FRONTEND = "Frontend";
+export const BACKEND = "Backend";
+export const FULL_STACK = "Full Stack";
+export const RESPONSIVE_DESIGN = "Responsive Design";
 
 export const projectTags = {
-  andrewKil: [SHOW_ALL, HTML5, CSS3, JAVASCRIPT, REACT, NETLIFY],
+  andrewKil: [
+    SHOW_ALL,
+    HTML5_CSS3,
+    JAVASCRIPT,
+    REACT,
+    NETLIFY,
+    FRONTEND,
+    RESPONSIVE_DESIGN,
+  ],
   saveAPlate: [
     SHOW_ALL,
-    HTML5,
-    CSS3,
+    HTML5_CSS3,
     JAVASCRIPT,
     REACT,
     REDUX,
     MATERIAL_UI,
-    NODE,
-    EXPRESS,
+    NODE_EXPRESS,
     POSTGRESQL,
     HEROKU,
+    FULL_STACK,
   ],
   sleepApp: [
     SHOW_ALL,
-    HTML5,
-    CSS3,
+    HTML5_CSS3,
     JAVASCRIPT,
     REACT,
     CONTEXT_API,
     MATERIAL_UI,
-    NODE,
-    EXPRESS,
+    NODE_EXPRESS,
     POSTGRESQL,
     FIREBASE,
+    FULL_STACK,
   ],
   scareTheCrow: [
     SHOW_ALL,
-    HTML5,
-    CSS3,
+    HTML5_CSS3,
     SASS_SCSS,
     JAVASCRIPT,
     REACT,
     LOCAL_STORAGE,
+    FRONTEND,
+    RESPONSIVE_DESIGN,
   ],
   covid19Tracker: [
     SHOW_ALL,
-    HTML5,
-    CSS3,
+    HTML5_CSS3,
     JAVASCRIPT,
     REACT,
     MATERIAL_UI,
     CHART_JS,
+    FRONTEND,
+    RESPONSIVE_DESIGN,
   ],
   connectFour: [
     SHOW_ALL,
-    HTML5,
-    CSS3,
+    HTML5_CSS3,
     CSS_MODULES,
     JAVASCRIPT,
     REACT,
     REDUX,
     BOOTSTRAP,
     JEST,
+    FRONTEND,
+    RESPONSIVE_DESIGN,
   ],
-  blackjack: [SHOW_ALL, HTML5, CSS3, JAVASCRIPT],
-  studyBuddy: [SHOW_ALL, HTML5, CSS3, JAVASCRIPT, REACT, LOCAL_STORAGE],
+  blackjack: [SHOW_ALL, HTML5_CSS3, JAVASCRIPT, FRONTEND],
+  studyBuddy: [
+    SHOW_ALL,
+    HTML5_CSS3,
+    JAVASCRIPT,
+    REACT,
+    LOCAL_STORAGE,
+    FRONTEND,
+    RESPONSIVE_DESIGN,
+  ],
 };
